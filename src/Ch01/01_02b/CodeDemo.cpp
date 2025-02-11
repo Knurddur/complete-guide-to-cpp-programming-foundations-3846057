181274870feee0547dcb743259236eec614db7ff
Knurddur <derek.elofson@gmail.com>
2025-02-10 19:16:21 -0500

    Changes to be committed: 	new file:   src/Ch01/01_02b/CodeDemo 	modified:   src/Ch01/01_02b/CodeDemo.cpp

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,19 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+/*
+#include <iostream>
+
+int main(){
+    std::cout << "Hi there nerd!" << std::endl;
+    std::cout << std::endl << std::endl;
+    return 0;
+}
+*/
+#include <cstdio>
+
+int main(){
+    printf("Hey asswipe\n\n");
+    return 0;
+}
